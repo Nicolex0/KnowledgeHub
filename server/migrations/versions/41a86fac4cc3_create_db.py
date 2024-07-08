5506fabdc4847fd2bf773c958eecf89950406ed3
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-07-08 21:06:00 +0300

    Use the template to set up the server directory

---
+++ server/migrations/versions/41a86fac4cc3_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: 41a86fac4cc3
+Revises: 
+Create Date: 2024-07-08 20:49:56.675934
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '41a86fac4cc3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
